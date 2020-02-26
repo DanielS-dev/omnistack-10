ccf83a5c93e9f9e230988a3a5ab2258c6a44ee6b
Daniel Silva <daniel.silva@insa.gov.br>
2020-02-26 16:28:58 -0300

    CORS adicionado

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,21 @@ const routes = require('./routes');
 
 const app = express();
 
-/*mongoose.connect('mongodb+srv://DanielSilva:y1xHJ6NhjF66g63v@cluster0-sw6xn.mongodb.net/week10?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://DanielSilva:y1xHJ6NhjF66g63v@cluster0-sw6xn.mongodb.net/week10?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});*/
+});
 
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true}) );
+
+app.all('/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+    next();
+})
 
 app.use(routes);
 
